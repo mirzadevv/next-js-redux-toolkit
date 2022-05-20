17434b54ab145cb21eb40534a4cdd039686f35bc
mirzaei.dev <mirzaei.devv@gmail.com>
2022-05-20 23:19:45 +0430

    create navBar component with theier links to todos, users and posts

---
+++ components/navBar.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import styles from "../styles/navBar.module.css";
+export default function NavBar() {
+  return (
+    <>
+      <div className={styles.navContainer}>
+        <ul>
+          <Link href="/todos">
+            <a>
+              <li>Todos (CSR)</li>
+            </a>
+          </Link>
+          <Link href="/users">
+            <a>
+              <li>Users (SSR)</li>
+            </a>
+          </Link>
+          <Link href="/posts">
+            <a>
+              <li>Posts (SSG)</li>
+            </a>
+          </Link>
+        </ul>
+      </div>
+    </>
+  );
+}
