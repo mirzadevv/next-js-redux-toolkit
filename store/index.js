e712530c87000fb3bc0c0ab1a39f3d8688ed8576
mirzaei.dev <mirzaei.devv@gmail.com>
2022-05-21 11:53:14 +0430

    feature: add usersReducers(usersSlices) to the store

--- store/index.js
+++ store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { createWrapper } from "next-redux-wrapper";
 import todosReducers from "./slices/todosSlices";
+import usersReducers from "./slices/usersSlices";
 
 const makeStore = () =>
   configureStore({
     reducer: {
       todos: todosReducers,
+      users: usersReducers,
     },
     devTools: true,
   });
